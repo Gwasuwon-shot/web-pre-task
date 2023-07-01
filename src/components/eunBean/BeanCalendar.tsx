7c4498ad830860cc45f37e23c159114ada3fd05e
Eunbeann <ds5bean23@gmail.com>
2023-07-01 15:21:08 +0900

    [ feat ] id 추가

--- src/components/eunBean/BeanCalendar.tsx
+++ src/components/eunBean/BeanCalendar.tsx
@@ -7,7 +7,42 @@ import Toolbar from "./customCalendar/Toolbar";
 export default function BeanCalendar() {
   moment.locale("ko-KR");
   const localizer = momentLocalizer(moment);
-  const myEventsList = [{ start: new Date(), end: new Date(), title: "special event" }];
+  const myEventsList = [
+    {
+      id: 0,
+      title: "은빈",
+      start: new Date(2023, 6, 1, 10, 0), // 시작 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      end: new Date(2023, 6, 1, 12, 0), // 종료 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      color: "#ff0000",
+    },
+    {
+      id: 1,
+      title: "늦은 은빈",
+      start: new Date(2023, 6, 1, 8, 0), // 시작 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      end: new Date(2023, 6, 1, 9, 0), // 종료 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      color: "#ff0000",
+    },
+    {
+      id: 2,
+      title: "3 은빈",
+      start: new Date(2023, 6, 1, 8, 0), // 시작 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      end: new Date(2023, 6, 1, 9, 0), // 종료 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      color: "#ff0000",
+    },
+    {
+      id: 3,
+      title: "4 은빈",
+      start: new Date(2023, 6, 1, 8, 0), // 시작 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      end: new Date(2023, 6, 1, 9, 0), // 종료 시간 (년, 월(0부터 시작), 일, 시간, 분)
+      color: "#ff0000",
+    },
+    {
+      id: 4,
+      title: "이벤트 2",
+      start: new Date(2023, 6, 2, 14, 0),
+      end: new Date(2023, 6, 2, 16, 0),
+    },
+  ];
 
   return (
     <>
@@ -17,6 +52,8 @@ export default function BeanCalendar() {
           localizer={localizer}
           style={{ height: 500, width: 500 }}
           events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
           components={{
             toolbar: Toolbar,
           }}

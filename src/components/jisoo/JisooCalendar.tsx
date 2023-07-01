6c84bb39d1fecf5ce737b6c905093c7c0f2b4d63
Seojisoo20191941 <amy0480@naver.com>
2023-07-02 06:37:43 +0900

    [ etc ] 변수명 변경

--- src/components/jisoo/JisooCalendar.tsx
+++ src/components/jisoo/JisooCalendar.tsx
@@ -7,15 +7,15 @@ import { CAELENDAR_DATA } from "../../core/jisoo/calendarData";
 import { calendarDatatTypes } from "../../type/jisoo/calendarDataTypes";
 
 export default function JisooCalendar() {
-  const [value, onChange] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [classData, setClassData] = useState<calendarDatatTypes[]>(CAELENDAR_DATA);
 
   return (
     <JisooCalendarWrapper>
       <Calendar
-        onChange={onChange} // useState로 포커스 변경 시 현재 날짜 받아오기
+        onChange={setSelectedDate} // useState로 포커스 변경 시 현재 날짜 받아오기
         formatDay={(locale, date) => moment(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
-        value={value}
+        value={selectedDate}
         minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
         maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
         showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정

2bb44be3505d0cef96f30ca92e35e1b3f3eb52e0
Eunbeann <ds5bean23@gmail.com>
2023-07-01 11:14:37 +0900

    [ feat ] 캘린더 초기 세팅

--- src/components/eunBean/BeanCalendar.tsx
+++ src/components/eunBean/BeanCalendar.tsx
@@ -1,11 +1,17 @@
 import { styled } from "styled-components";
-import "react-calendar/dist/Calendar.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function BeanCalendar() {
+  moment.locale("ko-KR");
+  const localizer = momentLocalizer(moment);
   return (
     <>
       <Header> 비니 캘린더 </Header>
-      <CalendarWrapper></CalendarWrapper>
+      <CalendarWrapper>
+        <Calendar localizer={localizer} style={{ height: 500 }} />
+      </CalendarWrapper>
     </>
   );
 }

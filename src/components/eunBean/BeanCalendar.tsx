8375bb9ebb52d536004ad8da78653bfd37761c77
Eunbeann <ds5bean23@gmail.com>
2023-07-01 13:07:02 +0900

    [ feat ] 캘린더 헤더 컴포넌트 추가, 기본 이벤트 추가

--- src/components/eunBean/BeanCalendar.tsx
+++ src/components/eunBean/BeanCalendar.tsx
@@ -2,15 +2,25 @@ import { styled } from "styled-components";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import Toolbar from "./customCalendar/Toolbar";
 
 export default function BeanCalendar() {
   moment.locale("ko-KR");
   const localizer = momentLocalizer(moment);
+  const myEventsList = [{ start: new Date(), end: new Date(), title: "special event" }];
+
   return (
     <>
       <Header> 비니 캘린더 </Header>
       <CalendarWrapper>
-        <Calendar localizer={localizer} style={{ height: 500 }} />
+        <Calendar
+          localizer={localizer}
+          style={{ height: 500 }}
+          components={{
+            toolbar: Toolbar,
+          }}
+          events={myEventsList}
+        />
       </CalendarWrapper>
     </>
   );
---
+++ src/components/eunBean/customCalendar/Toolbar.tsx
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+
+export default function Toolbar(props) {
+  const { date } = props;
+
+  const navigate = (action) => {
+    props.onNavigate(action);
+  };
+
+  return (
+    <CalendarWrapper>
+      {/* <div className="rbc-toolbar">
+        <span className="rbc-btn-group">
+          <button type="button" onClick={navigate.bind(null, "TODAY")}>
+            이번달
+          </button>
+          <button type="button" onClick={navigate.bind(null, "PREV")}>
+            이전
+          </button>
+          <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${
+            date.getMonth() + 1
+          }월`}</span>
+          <button type="button" onClick={navigate.bind(null, "NEXT")}>
+            다음
+          </button>
+        </span>
+      </div> */}
+    </CalendarWrapper>
+  );
+}
+
+const CalendarWrapper = styled.div`
+  .rbc-toolbar {
+    display: flex;
+    justify-content: space-around;
+    width: "500px";
+    background-color: lavender;
+  }
+`;

57f5d170c97e46c3294944d03b83d719fa2f9082
Eunbeann <ds5bean23@gmail.com>
2023-07-01 18:31:51 +0900

    [ feat ] 상수 데이터 추가

--- src/components/eunBean/customCalendar/Toolbar.tsx
+++ src/components/eunBean/customCalendar/Toolbar.tsx
@@ -8,30 +8,19 @@ export default function Toolbar(props) {
   };
 
   return (
-    <CalendarWrapper>
-      <div className="rbc-toolbar">
-        <span className="rbc-btn-group">
-          {/* <button type="button" onClick={navigate.bind(null, "TODAY")}>
+    <div className="rbc-toolbar">
+      {/* <span className="rbc-btn-group"> */}
+      {/* <button type="button" onClick={navigate.bind(null, "TODAY")}>
             이번달
           </button> */}
-          {/* <button type="button" onClick={navigate.bind(null, "PREV")}>
+      {/* <button type="button" onClick={navigate.bind(null, "PREV")}>
             이전
           </button> */}
-          <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
-          <button type="button" onClick={navigate.bind(null, "NEXT")}>
+      <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
+      {/* <button type="button" onClick={navigate.bind(null, "NEXT")}>
             다음
-          </button>
-        </span>
-      </div>
-    </CalendarWrapper>
+          </button> */}
+      {/* </span> */}
+    </div>
   );
 }
-
-const CalendarWrapper = styled.div`
-  .rbc-toolbar {
-    display: flex;
-    justify-content: space-around;
-    width: "500px";
-    background-color: lavender;
-  }
-`;

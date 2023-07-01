aeb619c24f849aa9727e66374fb0047a9c190866
Seojisoo20191941 <amy0480@naver.com>
2023-07-02 06:00:52 +0900

    [ etc ] 상수 파일 오류 수정

--- src/core/jisoo/calendarData.ts
+++ src/core/jisoo/calendarData.ts
@@ -3,7 +3,7 @@ import { calendarDatatTypes } from "../../type/jisoo/calendarDataTypes";
 export const CAELENDAR_DATA: calendarDatatTypes[] = [
   { id: 1, student: "은빈", time: "09:00~10:00", dates: ["2023-07-20"], color: "#FFC81E" },
   { id: 2, student: "혜인", time: "10:00~11:00", dates: ["2023-07-21"], color: "#EB0000" },
-  { id: 3, student: "성경", time: "9:00~10:00", dates: ["2023-07-06"], color: "#FFB6C1" },
+  { id: 3, student: "성경", time: "09:00~10:00", dates: ["2023-07-06"], color: "#FFB6C1" },
   { id: 4, student: "희정", time: "19:00~20:00", dates: ["2023-07-06", "2023-07-16"], color: "#7B68EE" },
   {
     id: 5,

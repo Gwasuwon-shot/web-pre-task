c199cb438346dffaa86470c150f50225d12ed429
Seojisoo20191941 <amy0480@naver.com>
2023-07-02 14:06:22 +0900

    [ feat ] 클릭된 날짜 보여지도록 값 가져오기

--- src/components/jisoo/JisooCalendar.tsx
+++ src/components/jisoo/JisooCalendar.tsx
@@ -7,41 +7,50 @@ import { CAELENDAR_DATA } from "../../core/jisoo/calendarData";
 import { calendarDatatTypes } from "../../type/jisoo/calendarDataTypes";
 
 export default function JisooCalendar() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [value, onChange] = useState<Date>(new Date());
   const [classData, setClassData] = useState<calendarDatatTypes[]>(CAELENDAR_DATA);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  function handleOpenNCloseModal() {
+    setOpenModal(!openModal);
+  }
 
   return (
-    <JisooCalendarWrapper>
-      <Calendar
-        onChange={setSelectedDate} // useState로 포커스 변경 시 현재 날짜 받아오기
-        formatDay={(locale, date) => moment(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
-        value={selectedDate}
-        minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-        maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
-        showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
-        className="mx-auto w-full text-sm border-b"
-        tileContent={({ date, view }) => {
-          // 날짜 타일에 컨텐츠 추가하기 (html 태그)
-          // 추가할 html 태그를 변수 초기화
-          let html: any[] = [];
-          // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
-          {
-            classData.map(
-              ({ id, student, time, dates, color }: calendarDatatTypes) =>
-                dates.find((x) => x === moment(date).format("YYYY-MM-DD")) &&
-                html.push(
-                  <Box key={id} $color={color}>
-                    <p>{student}</p>
-                    <p>{time}</p>
-                  </Box>,
-                ),
-            );
-          }
-          console.log(html);
-          return <>{html}</>;
-        }}
-      />
-    </JisooCalendarWrapper>
+    <>
+      <h1>{value.toISOString().substr(0, 10)}</h1>
+      <JisooCalendarWrapper onClick={handleOpenNCloseModal}>
+        <Calendar
+          onChange={onChange}
+          // useState로 포커스 변경 시 현재 날짜 받아오기
+          formatDay={(locale, date) => moment(date).format("DD")} // 날'일' 제외하고 숫자만 보이도록 설정
+          value={value}
+          minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+          maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+          showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
+          className="mx-auto w-full text-sm border-b"
+          tileContent={({ date, view }) => {
+            // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+            // 추가할 html 태그를 변수 초기화
+            let html: any[] = [];
+            // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
+            {
+              classData.map(
+                ({ id, student, time, dates, color }: calendarDatatTypes) =>
+                  dates.find((x) => x === moment(date).format("YYYY-MM-DD")) &&
+                  html.push(
+                    <Box key={id} $color={color}>
+                      <p>{student}</p>
+                      <p>{time}</p>
+                    </Box>,
+                  ),
+              );
+            }
+            console.log(html);
+            return <>{html}</>;
+          }}
+        />
+      </JisooCalendarWrapper>
+    </>
   );
 }
 

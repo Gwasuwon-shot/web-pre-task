041120269f40d475395bee23f7e5466d08aba19b
parkheeddong <phj7214go@gmail.com>
2023-07-04 14:58:28 +0900

    [ feat ] 달력에 날짜 정렬하여 추가 로직 구현

--- src/components/heeJung/Body.tsx
+++ src/components/heeJung/Body.tsx
@@ -20,32 +20,39 @@ export default function Body({currentMonth, schedule} :HeaderProps) {
     let day = startDate;
     let calenderDate = '';
     let isThisMonth = false; // 이번 달 날짜인지 확인하는 값
-    
+    let daySchedule = [];
+
     while (day <= endDate) {
 
         for (let i = 0; i < 7; i++) {
             // 이번달 날짜인지 검사 후, 이번달 날짜일 경우 스케줄 체크
-            if (isThisMonth === false && format(day, 'd') === '1') { 
+            if (isThisMonth === false && format(day, 'dd') === '01') { 
                 isThisMonth = true; // 지난달이었는데, 1일로 넘어오면 true 
-            } else if (isThisMonth === true && format(day, 'd') === '1') {
+            } else if (isThisMonth === true && format(day, 'dd') === '01') {
                 isThisMonth = false; // 이번달이었는데, 다음달로 넘어가면 false
             }
 
             if (isThisMonth === true) {
-                let compareDate = format(monthStart, 'yyyy.MM') + format(day, 'd');
+                let compareDate = format(monthStart, 'yyyy.MM') + "." + format(day, 'dd');
                 console.log(compareDate);
-                const daySchedule = schedule.filter((items) => items.date.includes(compareDate));
-                console.lo
+                daySchedule = schedule.filter((items) => items.date.includes(compareDate));
+                daySchedule = daySchedule.sort((a, b)=> a.time.localeCompare(b.time));
             }
 
-            const daySchedule = schedule.filter((item)=> item.date.includes(compareDate));
-            console.log(daySchedule);
 
             // calender 날짜 표기하는 부분
-            calenderDate = format(day, 'd');
+            calenderDate = format(day, 'dd');
             days.push(
                 <Day>
                     {calenderDate}
+                    {isThisMonth === true ? 
+                        daySchedule.map((item)=> (
+                            <ScheduleText>
+                                {item.student} {item.time}
+                            </ScheduleText>
+                        )) 
+                        : null
+                    }
                 </Day>
             );
             day = addDays(day, 1);
@@ -57,7 +64,7 @@ export default function Body({currentMonth, schedule} :HeaderProps) {
         );
         days = [];
     }
-    return <BodyWrapper>{rows}</BodyWrapper>;
+    return <BodyWrapper>{rows} </BodyWrapper>;
 };
 
 
@@ -75,12 +82,18 @@ const Row = styled.div`
 
 const Day = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 1.2rem;
+
+    width: 10rem;
+    height: 10rem;
+`
+
+const ScheduleText = styled.span`
+    display: flex;
     justify-content: center;
     align-items: center;
 
-    font-size: 1.2rem;
-
-    width: 3rem;
-    height: 7rem;
-
+    margin-top: 1rem;
 `

86e03e3ed9a478fcc4be438513c5c663cba6e6e3
Eunbeann <ds5bean23@gmail.com>
2023-07-05 17:23:15 +0900

    [ style ] 상수 데이터 수정

--- src/components/eunBean/customCalendar/CalendarConstnat.ts
+++ src/components/eunBean/customCalendar/CalendarConstnat.ts
@@ -14,80 +14,63 @@ export const EVENT_LIST = [
     title: "지수",
     start: new Date(2023, 6, 6, 13, 0), // 시작 시간 (년, 월(0부터 시작), 일, 시간, 분)
     end: new Date(2023, 6, 6, 14, 0), // 종료 시간 (년, 월(0부터 시작), 일, 시간, 분
-
-    // 각 컬러 지정을 위한 type 지정
     type: "복숭아",
+    // 각 컬러 지정을 위한 type 지정
   },
   {
     id: 1,
     title: "희정",
     start: new Date(2023, 6, 6, 19, 0),
     end: new Date(2023, 6, 6, 20, 0),
-    data: {
-      type: "키위",
-    },
+    type: "키위",
   },
   {
     id: 2,
     title: "은빈",
     start: new Date(2023, 6, 9, 18, 0),
     end: new Date(2023, 6, 9, 19, 0),
-    data: {
-      type: "토마토",
-    },
+    type: "토마토",
   },
   {
     id: 3,
     title: "지수",
     start: new Date(2023, 6, 13, 13, 0),
     end: new Date(2023, 6, 13, 14, 0),
-    data: {
-      type: "복숭아",
-    },
+    type: "복숭아",
   },
   {
     id: 4,
     title: "희정",
     start: new Date(2023, 6, 16, 16, 0),
     end: new Date(2023, 6, 16, 24, 0),
-    data: {
-      type: "키위",
-    },
+    type: "키위",
   },
   {
     id: 5,
     title: "지수",
     start: new Date(2023, 6, 20, 13, 0),
     end: new Date(2023, 6, 20, 14, 0),
-    data: {
-      type: "복숭아",
-    },
+    type: "복숭아",
   },
   {
     id: 5,
     title: "성경",
     start: new Date(2023, 6, 22, 11, 0),
     end: new Date(2023, 6, 22, 12, 0),
-    data: {
-      type: "체리",
-    },
+    type: "체리",
   },
   {
     id: 5,
     title: "지수",
     start: new Date(2023, 6, 27, 13, 0),
     end: new Date(2023, 6, 27, 14, 0),
-    data: {
-      type: "복숭아",
-    },
+    type: "복숭아",
   },
   {
     id: 6,
     title: "혜인",
     start: new Date(2023, 6, 30, 15, 0),
     end: new Date(2023, 6, 30, 16, 0),
-    data: {
-      type: "바나나",
-    },
+    type: "바나나",
   },
 ];

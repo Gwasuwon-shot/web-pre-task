80ff87ec978543d565ee3c80683fad96d4572b5e
Seojisoo20191941 <amy0480@naver.com>
2023-07-06 17:43:03 +0900

    [ feat ] 알림 pull 로직 구현

--- src/api/jisooAlarm.ts
+++
@@ -1,5 +0,0 @@
-// import { getToken } from "firebase/messaging";
-
-// const token = await getToken(messaging, {
-//   vapidKey: `${import.meta.env.VITE_APP_VAPID_KEY}`,
-// });
--- src/components/jisoo/JisooAlarm.tsx
+++ src/components/jisoo/JisooAlarm.tsx
@@ -1,7 +1,11 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import styled from "styled-components";
+import { getToken } from "../../api/getAlarmToken";
+import { pushAlarm } from "../../api/pushAlarm";
 
 export default function JisooAlarm() {
-  function handleAttend() {
+  // 알림 허용하는 버튼
+  function handleAllowAlarm() {
     Notification.requestPermission().then((permission) => {
       if (permission === "granted") {
         console.log("알림 권한이 허용됨");
@@ -13,7 +17,37 @@ export default function JisooAlarm() {
     });
   }
 
-  return <AlarmButton onClick={handleAttend}>출석</AlarmButton>;
+  // async 를 사용학 위해 메서드로 따로 분리함
+  //   async function handleAttend() {
+  //     let token = await getToken().
+  //     // console.log("token === ", token);
+
+  //     //서버에 토큰 보내기
+  //     let data = await pushAlarm(token);
+  //     console.log(data);
+  //   }
+
+  const queryClient = useQueryClient();
+
+  function handleAttend() {
+    queryClient.invalidateQueries("getDeviceToken");
+  }
+
+  const { data: getDeviceToken } = useQuery("getDeviceToken", getToken(), {
+    onSuccess: (res) => {
+      console.log(res);
+      //   pushAlarmStart(token);
+    },
+  });
+
+  const { mutate: pushAlarmStart } = useMutation(pushAlarm);
+
+  return (
+    <>
+      <button onClick={handleAllowAlarm}>알림 허용</button>
+      <AlarmButton onClick={handleAttend}>출석</AlarmButton>
+    </>
+  );
 }
 
 const AlarmButton = styled.button`

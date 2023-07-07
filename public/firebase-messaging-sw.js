2c6f16469a5b468764fe6b586c4a8f26038cd02e
HAE2NI <hae0_02ni@ewhain.net>
2023-07-08 00:38:28 +0900

    [ hotfix ] 수정,,무엇을? firebaseConfig를,,

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
 const firebaseConfig = {
-  apiKey: "AIzaSyDNyoLsnDzmay7XS2uB_CIIwuKGx26ZeRk",
+  apiKey: import.meta.env.REACT_APP_API_KEY,
   authDomain: "pre-gwasuoneshot.firebaseapp.com",
   projectId: "pre-gwasuoneshot",
   storageBucket: "pre-gwasuoneshot.appspot.com",
-  messagingSenderId: "137792399262",
-  appId: "1:137792399262:web:9674770e2ca7cb6212988f",
-  measurementId: "G-T2QDBQ3LHT",
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_ID,
+  appId: import.meta.env.REACT_APP_APP_ID,
+  measurementId: import.meta.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);

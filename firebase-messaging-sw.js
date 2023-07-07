3ffea3b1508fa7f642d9763639a13dc1d2e98606
HAE2NI <hae0_02ni@ewhain.net>
2023-07-08 00:32:54 +0900

    [ hotfix ] 수정

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,46 +1,35 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
-import "@firebase/messaging";
-
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
+import "@firebase/messaging";
+// import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDNyoLsnDzmay7XS2uB_CIIwuKGx26ZeRk",
+  apiKey: import.meta.env.REACT_APP_API_KEY,
   authDomain: "pre-gwasuoneshot.firebaseapp.com",
   projectId: "pre-gwasuoneshot",
   storageBucket: "pre-gwasuoneshot.appspot.com",
-  messagingSenderId: "137792399262",
-  appId: "1:137792399262:web:9674770e2ca7cb6212988f",
-  measurementId: "G-T2QDBQ3LHT",
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_ID,
+  appId: import.meta.env.REACT_APP_APP_ID,
+  measurementId: import.meta.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const messaging = getMessaging();
 
-async function requestPermission() {
-  console.log("권한 요청 즁");
+getToken(messaging, {
+  vapidKey: import.meta.env.REACT_APP_VAPID_KEY,
+})
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log("currentToken", currentToken);
+    } else {
+      console.log("No registration");
+    }
+  })
+  .catch((e) => console.log("err:", e));
 
-  const permission = await Notification.requestPermission();
-  if (permission === "denied") {
-    console.log("알림 허용 안됨");
-    return;
-  }
-
-  console.log("알림 권한이 허용됨");
-
-  const token = await getToken(messaging, {
-    vapidKey: import.meta.env.REACT_APP_VAPID_KEY,
-  });
-
-  if (token) console.log("token:", token);
-  else console.log("Can not get Token");
-
-  onMessage(messaging, (payload) => {
-    console.log("메세지가 도착했습니다.", payload);
-  });
-}
-
-requestPermission();
+onMessage(messaging, (payload) => {
+  console.log("Message received", payload);
+});
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,31 +1,30 @@
-self.addEventListener("install", function (e) {
-  console.log("fcm sw install,,,");
-  self.skipWaiting();
-});
+// importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+const firebaseConfig = {
+  apiKey: "AIzaSyDNyoLsnDzmay7XS2uB_CIIwuKGx26ZeRk",
+  authDomain: "pre-gwasuoneshot.firebaseapp.com",
+  projectId: "pre-gwasuoneshot",
+  storageBucket: "pre-gwasuoneshot.appspot.com",
+  messagingSenderId: "137792399262",
+  appId: "1:137792399262:web:9674770e2ca7cb6212988f",
+  measurementId: "G-T2QDBQ3LHT",
+};
 
-self.addEventListener("activate", function (e) {
-  console.log("fcm sw activate,,,");
-});
+firebase.initializeApp(firebaseConfig);
 
-self.addEventListener("push", function (e) {
-  console.log("push:", e.data.json());
-  if (!e.data.json()) return;
+const messaging = firebase.messaging();
 
-  const resultData = e.data.json().notification;
-  const notificationTitle = resultData.title;
+messaging.onBackgroundMessage(messaging, (payload) => {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+
+  // Customize notification here
+  const notificationTitle = "Tutice";
   const notificationOptions = {
-    body: resultData.body,
-    ...resultData,
+    body: payload,
+    icon: "/fruit.png",
   };
-  console.log("push", { resultData, notificationTitle, notificationOptions });
 
   self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-self.addEventListener("notificationclick", function (e) {
-  console.log("notification click");
-
-  const url = "/";
-  e.notification.close();
-  e.waitUntil(clients.openWindow(url));
 });

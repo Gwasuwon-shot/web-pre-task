6cba0572bb1b07ae881df301bd5d8c226793bedb
Seojisoo20191941 <amy0480@naver.com>
2023-07-07 16:00:58 +0900

    [ fix ] 컴포넌트 open 안 한 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Router from "./Router";
 import { GlobalStyle } from "./style/globalStyle";
 import { theme } from "./style/theme";
 
+export default function App() {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>

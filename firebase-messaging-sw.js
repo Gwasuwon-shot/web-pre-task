19d2b6213f3c2eaeaa3a6967a283a4270ae68c19
Seojisoo20191941 <amy0480@naver.com>
2023-07-07 21:46:53 +0900

    [ feat ] resultData 확인

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,18 +10,20 @@ self.addEventListener("activate", function (e) {
 });
 
 self.addEventListener("push", function (e) {
-  console.log("push: ", e.data.json());
+  // console.log("push: ", e.data.json());
   if (!e.data.json()) return;
 
   const resultData = e.data.json().notification;
   const notificationTitle = resultData.title;
+
+  console.log(resultData);
   const notificationOptions = {
     body: resultData.body,
     icon: "/tutice.png",
     // tag: resultData.tag,
     // ...resultData,
   };
-  console.log("notificationOptions: ", { notificationTitle, notificationOptions });
+  // console.log("notificationOptions: ", { notificationTitle, notificationOptions });
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
